087e2a381954c0a52cf8849a4d6c8759f7b78782
shoaib3474 <shoaib3974@gmail.com>
2025-12-18 02:28:41 +0500

    change the design of nav_bar

--- lib/app/modules/global_widgets/custom_bottom_nav_bar.dart
+++ lib/app/modules/global_widgets/custom_bottom_nav_bar.dart
@@ -7,7 +7,9 @@
 
 import 'package:flutter/material.dart';
 
+// ignore: constant_identifier_names
 const Color PRIMARY_COLOR = Colors.blueAccent;
+// ignore: constant_identifier_names
 const Color BACKGROUND_COLOR = Color(0xffE2E7F2);
 
 class CustomBottomNavigationBar extends StatefulWidget {
@@ -17,16 +19,24 @@ class CustomBottomNavigationBar extends StatefulWidget {
   final Function(int) onChange;
   final int currentIndex;
 
-  CustomBottomNavigationBar({this.backgroundColor = BACKGROUND_COLOR, this.itemColor = PRIMARY_COLOR, this.currentIndex = 0, required this.children, required this.onChange});
+  const CustomBottomNavigationBar(
+      {super.key,
+      this.backgroundColor = BACKGROUND_COLOR,
+      this.itemColor = PRIMARY_COLOR,
+      this.currentIndex = 0,
+      required this.children,
+      required this.onChange});
 
   @override
-  _CustomBottomNavigationBarState createState() => _CustomBottomNavigationBarState();
+  // ignore: library_private_types_in_public_api
+  _CustomBottomNavigationBarState createState() =>
+      _CustomBottomNavigationBarState();
 }
 
 class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
   void _changeIndex(int index) {
     widget.onChange(index);
-    }
+  }
 
   @override
   void initState() {
@@ -35,51 +45,79 @@ class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 60,
-      color: widget.backgroundColor,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: widget.children.map((item) {
-          var color = item.color ?? widget.itemColor;
-          var icon = item.icon;
-          var label = item.label;
-          int index = widget.children.indexOf(item);
-          return GestureDetector(
-            onTap: () {
-              _changeIndex(index);
-            },
-            child: AnimatedContainer(
-              duration: Duration(milliseconds: 200),
-              width: widget.currentIndex == index ? MediaQuery.of(context).size.width / widget.children.length + 20 : 50,
-              padding: EdgeInsets.only(left: 10, right: 10),
-              margin: EdgeInsets.only(top: 10, bottom: 10),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(color: widget.currentIndex == index ? color : Colors.transparent, borderRadius: BorderRadius.circular(10)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Icon(
-                    icon,
-                    size: 24,
-                    color: widget.currentIndex == index ? Theme.of(context).primaryColor : color,
-                  ),
-                  widget.currentIndex == index
-                      ? Expanded(
-                          flex: 2,
-                          child: Text(
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Container(
+        height: 70,
+        decoration: BoxDecoration(
+          color: widget.backgroundColor,
+          borderRadius: BorderRadius.circular(32),
+          boxShadow: [
+            BoxShadow(
+              // ignore: deprecated_member_use
+              color: Colors.black12.withOpacity(0.1),
+              blurRadius: 10,
+              offset: const Offset(0, 4),
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: widget.children.map((item) {
+            var color = item.color ?? widget.itemColor;
+            var icon = item.icon;
+            var label = item.label;
+            int index = widget.children.indexOf(item);
+            return GestureDetector(
+              onTap: () {
+                _changeIndex(index);
+              },
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 200),
+                width: widget.currentIndex == index
+                    ? MediaQuery.of(context).size.width /
+                            widget.children.length +
+                        20
+                    : 60,
+                // padding:
+                //     const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                margin: const EdgeInsets.only(top: 8, bottom: 8),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: widget.currentIndex == index
+                        ? (item.color ?? widget.itemColor)
+                        : Colors.transparent,
+                    borderRadius: BorderRadius.circular(20)),
+                child: widget.currentIndex == index
+                    ? Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            icon,
+                            size: 22,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
                             label,
                             overflow: TextOverflow.ellipsis,
                             textAlign: TextAlign.center,
-                            style: TextStyle(color: Theme.of(context).primaryColor),
+                            style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                fontSize: 12),
                           ),
-                        )
-                      : Container()
-                ],
+                        ],
+                      )
+                    : Icon(
+                        icon,
+                        size: 24,
+                        color: color,
+                      ),
               ),
-            ),
-          );
-        }).toList(),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
@@ -90,5 +128,6 @@ class CustomBottomNavigationItem {
   final String label;
   final Color? color;
 
-  CustomBottomNavigationItem({required this.icon, required this.label, this.color});
+  CustomBottomNavigationItem(
+      {required this.icon, required this.label, this.color});
 }
